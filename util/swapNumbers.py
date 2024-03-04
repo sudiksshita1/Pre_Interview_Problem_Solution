1ca0dc4d4c30a21e3c882eef159008acdbe14cf2
sudiksshita1 <sudiksshita.s.samall@gmail.com>
2024-03-04 10:21:59 +0530

    Changes to the file swap.py

--- util/swapNumbers.py
+++ util/swapNumbers.py
@@ -1,3 +1,16 @@
 # Swapping the values of a and b using reference passing
-def swap_variables(list):
-    list[0], list[1] = list[1], list[0]
+#def swap_variables(list):
+    #list[0], list[1] = list[1], list[0]
+    #list[0], list[1] = list[1], list[0]
+def swap_variables(a, b):
+    a,b = b, a
+if __name__ == "__main__":
+    print("Before swapping:")
+    print("a =", a)
+    print("b =", b)
+
+swap_variables(a, b)
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
